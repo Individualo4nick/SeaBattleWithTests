22b9236ba19ef159b6d36a8716dbc0bd2536d1fd
vladv <vlad.vlasov77@mail.ru>
2023-10-10 22:39:35 +0300

    add Vlasov's BDD tests

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -3,6 +3,7 @@ package org.example;
 public class Game {
     public int polePlayer[][];
     public int poleComp[][];
+    public int C1, C2, C3, C4;
     public Game() {
         polePlayer = new int[10][10];
         poleComp = new int[10][10];
@@ -18,5 +19,17 @@ public class Game {
         if (((i >= 0) && (i <= 9)) && ((j >= 0) && (j <= 9))) {
             return true;
         } else return false;
+    }
+    public void kolvoUbitPlayer(int[][]mas) {
+        C4 = 0;C3 = 0;C2 = 0;C1 = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (mas[i][j] == 18) C4 = (C4 + 1);
+                if (mas[i][j] == 17) C3 = (C3 + 1);
+                if (mas[i][j] == 16) C2 = (C2 + 1);
+                if (mas[i][j] == 15) C1 = (C1 + 1);
+            }
+        }
+        C4/=4;C3/=3;C2/=2;
     }
 }
---
+++ src/test/java/steps/KolvoUbitStep.java
@@ -0,0 +1,78 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.example.Game;
+import org.junit.Assert;
+
+public class KolvoUbitStep {
+    private Game game;
+    private int C1;
+    private int C2;
+    private int C3;
+    private int C4;
+    @Given("Дано игровое поле, с подбитыми кораблями")
+    public void setNewGame(){
+        game = new Game();
+    }
+
+    @Given("Количество подбитых четырехпарусных {int}, трехпарусных {int}, двухпарусных {int}, однопарусных {int}")
+    public void setExpectedCount(int arg0, int arg1, int arg2, int arg3) {
+        C4 = arg0;
+        C3 = arg1;
+        C2 = arg2;
+        C1 = arg3;
+        int[][] field = game.polePlayer;
+        //Подбитые четырехпалубные корабли
+        int index = 0;
+        for(int i =0 ; i < C4; i++) {
+            for(int j = 0; j < 4; j++) {
+                field[index][j] = 18;
+                index += 2;
+            }
+        }
+        //Трехпарусные корабли
+        index = 2;
+        for(int i = 0; i < C3; i++ ) {
+            for(int j =0 ; j < 3; j++) {
+                field[index][j] = 17;
+                index += 2;
+            }
+        }
+        //Двухпарусные корабли
+        index = 0;
+        for(int i =0; i < C2; i++) {
+            for(int j = 5; j <= 6; j++) {
+                field[index][j] = 16;
+                index += 2;
+            }
+        }
+
+        // Однопарусные корабли
+        index = 0;
+        for(int i = 0; i < C1; i++) {
+            field[7][index] = 15;
+            index += 2;
+        }
+    }
+    @When("Количество указано корректно")
+    public void checkTestValues() {
+        Assert.assertTrue(C1 <= 4);
+        Assert.assertTrue(C2 <= 3);
+        Assert.assertTrue(C3 <= 2);
+        Assert.assertTrue(C4 <= 1);
+
+    }
+
+    @Then("Получаем корректное значение подбитых кораблей каждого типа")
+    public void getResult() {
+        game.kolvoUbitPlayer(game.polePlayer);
+        Assert.assertEquals(game.C1, C1);
+        Assert.assertEquals(game.C2, C2);
+        Assert.assertEquals(game.C3, C3);
+        Assert.assertEquals(game.C4, C4);
+    }
+
+
+}

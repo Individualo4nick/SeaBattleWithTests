76e4b619bad69b79c81f3e5a786eae8f5153a464
Vitaliy <modecom.pirogenko@gmail.com>
2023-10-10 21:32:50 +0300

    add function for bdd test

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -7,4 +7,16 @@ public class Game {
         polePlayer = new int[10][10];
         poleComp = new int[10][10];
     }
+    public void setOkrKilled(int mas[][], int i, int j){
+        if (indOutOfBounds(i, j)){
+            if (mas[i][j]==-1 || mas[i][j]==6){
+                mas[i][j]--;
+            }
+        }
+    }
+    private boolean indOutOfBounds(int i, int j) {
+        if (((i >= 0) && (i <= 9)) && ((j >= 0) && (j <= 9))) {
+            return true;
+        } else return false;
+    }
 }

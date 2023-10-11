68ce481e19e2890453682b5dbd611fa06bf96b70
vladv <vlad.vlasov77@mail.ru>
2023-10-11 11:34:05 +0300

    add Vlasov's function for tdd tests

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -32,4 +32,44 @@ public class Game {
         }
         C4/=4;C3/=3;C2/=2;
     }
+
+    public void proverkaNaUbiistvo(int pole[][], int i, int j, int kolpalub) {
+        //Количество раненых палуб
+        int ranen=0;
+        //Выполняем подсчет раненых палуб
+        for (int a=i-(kolpalub-1);a<=i+(kolpalub-1  );a++) {
+            for (int b = j - (kolpalub - 1); b <= j + (kolpalub - 1); b++) {
+                // Если это палуба раненого корабля
+                if (indOutOfBounds(a, b) && (pole[a][b] == kolpalub + 7))
+                    ranen++;
+            }
+        }
+        // Если количество раненых палуб совпадает с количеством палуб
+        // корабля, то он убит - прибавляем число 7
+        if (ranen==kolpalub) {
+            for (int x=i-(kolpalub-1);x<=i+(kolpalub-1);x++) {
+                for (int y=j-(kolpalub-1);y<=j+(kolpalub-1);y++) {
+                    // Если это палуба раненого корабля
+                    if (indOutOfBounds(x, y)&&(pole[x][y]==kolpalub+7)) {
+                        // помечаем палубой убитого корабля
+                        pole[x][y]+=7;
+                        // уменьшаем на 1 окружение убитого корабля
+                        surroundADeadShip(pole, x, y);
+                    }
+                }
+            }
+        }
+
+
+    }
+    public void surroundADeadShip(int[][] mas, int i, int j) {
+        setOkrKilled(mas, i - 1, j - 1); // сверху слева
+        setOkrKilled(mas, i - 1, j); // сверху
+        setOkrKilled(mas, i - 1, j + 1); // сверху справа
+        setOkrKilled(mas, i, j + 1); // справа
+        setOkrKilled(mas, i + 1, j + 1); // снизу справа
+        setOkrKilled(mas, i + 1, j); // снизу
+        setOkrKilled(mas, i + 1, j - 1); // снизу слева
+        setOkrKilled(mas, i, j - 1); // слева
+    }
 }
